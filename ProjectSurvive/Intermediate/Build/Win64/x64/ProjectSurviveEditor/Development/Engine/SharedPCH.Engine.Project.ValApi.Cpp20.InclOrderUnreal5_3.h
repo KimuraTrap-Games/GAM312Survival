cc80a04a2fff3642bf184e364bb42db685d82921
Scott Lee <scottlee5455@outlook.com>
2025-05-11 10:54:38 -0500

    setup the project settings

---
+++ ProjectSurvive/Intermediate/Build/Win64/x64/ProjectSurviveEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Users/scott/Desktop/GAM312Survival/ProjectSurvive/Intermediate/Build/Win64/x64/ProjectSurviveEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ ProjectSurvive/Intermediate/Build/Win64/x64/ProjectSurviveEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Users/scott/Desktop/GAM312Survival/ProjectSurvive/Intermediate/Build/Win64/x64/ProjectSurviveEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
